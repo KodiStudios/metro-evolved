7e548b35cdac4b901dec6a1b8f4e74b50877f8eb
Nikolai Aristov <KodiStudios@gmail.com>
2024-03-22 22:29:45 -0700

    Revert

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/metro-evolved/",
 
   trailingSlash: false,
 
