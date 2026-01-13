cbf05218abfeec5a25a83d1709433615822ceb8a
KodiStudios <60270547+KodiStudios@users.noreply.github.com>
2026-01-12 21:19:38 -0800

    Fix Kodi Terminal name (#15)
    * Update terminal settings for PowerShell compatibility and add missing spell check words
    
    * Refactor content formatting in Amazon Spheres article and update Docusaurus config for markdown link handling
    
    ---------
    
    Co-authored-by: Nikolai Aristov <kodistudios@mgmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,6 @@ const config: Config = {
   //projectName: "metro-evolved", // Usually your repo name.
 
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -30,6 +29,12 @@ const config: Config = {
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
+  },
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
   },
 
   presets: [
