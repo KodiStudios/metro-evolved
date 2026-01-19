648eff037df7b7d1a7d2001521c83d5aac23f5dc
KodiStudios <60270547+KodiStudios@users.noreply.github.com>
2026-01-19 11:59:10 -0800

    Remove docu graphs (#19)
    * Update dependencies to latest versions in package.json and yarn.lock
    
    * Fix update script in package.json to include wildcard for all packages
    
    * Update version number to 0.26.1 in package.json
    
    * Remove outdated Windows Terminal documentation and configuration files; add new sidebar configuration example
    
    * Refactor configuration and update dependencies for compatibility with Docusaurus 3
    
    * Refactor HomepageFeatures component to make SVG icons optional and center align feature items
    
    * Remove unused SVG file and update dependencies in yarn.lock to include @types/mdast@4.0.4
    
    * Update loose-envify version range in yarn.lock
    
    ---------
    
    Co-authored-by: Nikolai Aristov <kodistudios@mgmail.com>

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,45 +1,20 @@
 import clsx from "clsx";
 import Heading from "@theme/Heading";
 import styles from "./styles.module.css";
-import MountainSvg from "@site/static/img/undraw_docusaurus_mountain.svg";
-import TreeSvg from "@site/static/img/undraw_docusaurus_tree.svg";
-import ReactSvg from "@site/static/img/undraw_docusaurus_react.svg";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  Svg?: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
     title: "Easy to Use AI",
-    Svg: MountainSvg,
     description: (
       <>
         Using generative AI for fun and profit, from creating original artwork
         to deciding what to make for lunch, offers exciting possibilities
-      </>
-    ),
-  },
-  {
-    title: "Focus on What Matters",
-    Svg: TreeSvg,
-    description: (
-      <>
-        Generative AI can enhance focus by automating repetitive tasks, freeing
-        up mental bandwidth for more creative and critical thinking
-      </>
-    ),
-  },
-  {
-    title: "Powered by AI",
-    Svg: ReactSvg,
-    description: (
-      <>
-        Generative AI tools possess remarkable power and flexibility. Their
-        ability to learn and evolve makes them indispensable for innovation and
-        problem-solving
       </>
     ),
   },
@@ -48,9 +23,11 @@ const FeatureList: FeatureItem[] = [
 function Feature({ title, Svg, description }: FeatureItem) {
   return (
     <div className={clsx("col col--4")}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
+      {Svg && (
+        <div className="text--center">
+          <Svg className={styles.featureSvg} role="img" />
+        </div>
+      )}
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
@@ -63,7 +40,7 @@ export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className="row" style={{ justifyContent: "center" }}>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
